1de528aac10163695fab17a24660f75639e4d32f
Erik Rodrigues <erik.webprogrammer@gmail.com>
2025-06-09 18:42:44 -0300

    feature: persist messages to db

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,6 +3,9 @@ import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
+from django.contrib.auth.models import User
+from .models import ChatRoom, Message
+
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
@@ -23,12 +26,31 @@ class ChatConsumer(WebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
+        sender = User.objects.get(username="anon")
+
+        room, _ = ChatRoom.objects.get_or_create(name=self.room_name)
+
+        Message.objects.create(
+            room=room,
+            sender=sender,
+            content=message
+        )
+
         async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat.message", "message": message}
+            self.room_group_name, 
+            {
+                "type": "chat.message",
+                "message": message,
+                "sender": sender.username
+            }
         )
 
     def chat_message(self, event):
         message = event["message"]
+        sender = event["sender"]
 
-        self.send(text_data=json.dumps({"message": message}))
+        self.send(text_data=json.dumps({
+            "message": message,
+            "sender": sender
+        }))
     
