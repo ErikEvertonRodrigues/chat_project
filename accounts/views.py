fc3294fe4e61977616c52ce2944be9bf19e4cf64
Erik Rodrigues <erik.webprogrammer@gmail.com>
2025-06-11 19:21:00 -0300

    feature: add user login logic

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 from .forms import CustomUserCreationForm
 
 # Create your views here.
@@ -12,3 +14,16 @@ def register(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Invalid credentials')
+    return render(request, 'accounts/login.html')
