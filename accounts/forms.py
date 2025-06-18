91f0205471dcd39d0b7040309c2994ccb209f7ab
Erik Rodrigues <erik.webprogrammer@gmail.com>
2025-06-18 19:01:37 -0300

    add styles to register page

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,8 +3,27 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
+        'class': 'w-full p-2 outline outline-blue-600 rounded-[5px]',
+        'placeholder': 'Email',
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'w-full p-2 outline outline-blue-600 rounded-[5px]',
+        'placeholder': 'Password',
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'w-full p-2 outline outline-blue-600 rounded-[5px]',
+        'placeholder': 'Password again',
+    }))
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'w-full p-2 outline outline-blue-600 rounded-[5px]',
+                'placeholder': 'Username',
+            }),
+        }
