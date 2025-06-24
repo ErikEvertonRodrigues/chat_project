950728fe119c3152b7368eb4b0314c64d5ec06fc
Erik Rodrigues <erik.webprogrammer@gmail.com>
2025-06-24 15:03:28 -0300

    fix: change date formart to Brazil format

--- static/js/room.js
+++ static/js/room.js
@@ -26,7 +26,7 @@ function appendMessage(sender, message, timestamp, isHistory) {
     const div = document.createElement("div");
     //After: Add logic with isHistory, but for now let's make it simple
 
-    div.innerHTML = `${sender}: ${message} <br> ${timestamp}`
+    div.innerHTML = `${sender}: ${message} <br> ${formatTimeStamp(timestamp)}`
     div.classList.add("bg-room-base", "inline-block", "w-fit", "rounded-lg", "p-2", "text-white");
     container.appendChild(div)
 }
@@ -50,3 +50,22 @@ document.querySelector('#chat-message-submit').onclick = function(e) {
     }));
     messageInputDom.value = '';
 };
+
+function formatTimeStamp(isoString) {
+    const date = new Date(isoString);
+
+    const options = {
+        timeZone: 'America/Sao_Paulo',
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false,
+    };
+
+    const formatter = new Intl.DateTimeFormat('pt-BR', options);
+    const formatted = formatter.format(date);
+
+    return formatted + 'h';
+}
