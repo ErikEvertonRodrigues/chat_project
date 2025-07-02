b22091fa4fb8852c3e8dcf0c29b80a052f222ad2
Erik Rodrigues <erik.webprogrammer@gmail.com>
2025-07-02 18:49:00 -0300

    Feature: Add user profile module

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="User Account")
+    avatar = models.ImageField(upload_to="profile/avatar/", null=True, blank=True, verbose_name="User Biography")
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
