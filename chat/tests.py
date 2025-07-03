586ff1e15306725bba4fae0edfb25c9f03c60236
Erik Rodrigues <erik.webprogrammer@gmail.com>
2025-07-03 16:12:27 -0300

    Add test for creating ChatRoom

--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import ChatRoom
 
 # Create your tests here.
+
+class ChatRoomModelTest(TestCase):
+    def test_create_room(self):
+        room_name = "testroom"
+        room = ChatRoom.objects.create(name=room_name)
+        self.assertEqual(room.name, room_name)
+        self.assertIsNotNone(room.created_at)
