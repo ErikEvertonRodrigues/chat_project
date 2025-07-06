e5022ad800fb72392fc59dc7d14bd945d6d9a4b5
Erik Rodrigues <erik.webprogrammer@gmail.com>
2025-07-06 14:59:54 -0300

    test: ensure Message model creates message linked to room and sender

--- chat/tests/test_chat.py
+++ chat/tests/test_chat.py
@@ -1,12 +1,24 @@
-from ..models import ChatRoom
+from django.contrib.auth import get_user_model
+from ..models import ChatRoom, Message
 import pytest
 
 # Create your tests here.
-
+user_model = get_user_model()
+room_name = "room-name-test"
 @pytest.mark.django_db
 def test_create_room():
-    room_name = "room-name-test"
     room = ChatRoom.objects.create(name=room_name)
     assert ChatRoom.objects.count() == 1
     assert room.name == room_name
+
+@pytest.mark.django_db
+def test_create_message():
+    room = ChatRoom.objects.create(name=room_name)
+    user = user_model.objects.create(username="test", password="test")
+    content = "This is a test message"
+    message = Message.objects.create(room=room, sender=user, content=content)
+    assert Message.objects.count() == 1
+    assert message.room.name == room_name
+    assert message.sender == user
+    assert message.content == content
 
